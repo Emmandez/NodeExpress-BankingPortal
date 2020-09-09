66dc6d6c67283820db2892ac14fc251ed5c53fb6
Emmanuel <eherdezmoran@gmail.com>
2020-09-08 21:24:45 -0500

    Module 6 done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 const app = express();
 
@@ -10,6 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/', (req, res) => {
 
@@ -19,65 +23,9 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/savings', (req, res) => {
-    res.render('account', { account: accounts.savings });
-});
-
-app.get('/checking', (req, res) => {
-    res.render('account', { account: accounts.checking });
-});
-
-app.get('/credit', (req, res) => {
-    res.render('account', { account: accounts.credit });
-});
-
 app.get('/profile', (req, res) => {
     res.render('profile', { user: users[0] });
 });
-
-app.get('/transfer', (req, res) => {
-    res.render('transfer');
-});
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance -= parseInt(req.body.amount);
-    accounts[req.body.to].balance += parseInt(req.body.amount);
-
-    writeJSON();
-
-    res.render('transfer', {
-        message: "Transfer Completed"
-    })
-
-});
-
-app.get('/payment', (req, res) => {
-    res.render('payment', {
-        account: accounts.credit
-    });
-});
-
-app.post('/payment', (req, res) => {
-
-    let amount = req.body.amount;
-    accounts.credit.balance -= parseInt(amount);
-    accounts.credit.available += parseInt(amount);
-
-    writeJSON();
-
-    res.render('payment', {
-        message: "Payment Successful",
-        account: accounts.credit
-    });
-});
-
-
-
-
-
-
-
-
 
 app.listen(3000, () => {
     console.log("PS Project Running on port 3000!");
---
+++ src/routes/services.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { accounts, writeJSON } = require('../data');
+const router = express.Router();
+
+
+
+router.get('/transfer', (req, res) => {
+    res.render('transfer');
+});
+
+router.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance -= parseInt(req.body.amount);
+    accounts[req.body.to].balance += parseInt(req.body.amount);
+
+    writeJSON();
+
+    res.render('transfer', {
+        message: "Transfer Completed"
+    })
+
+});
+
+router.get('/payment', (req, res) => {
+    res.render('payment', {
+        account: accounts.credit
+    });
+});
+
+router.post('/payment', (req, res) => {
+
+    let amount = req.body.amount;
+    accounts.credit.balance -= parseInt(amount);
+    accounts.credit.available += parseInt(amount);
+
+    writeJSON();
+
+    res.render('payment', {
+        message: "Payment Successful",
+        account: accounts.credit
+    });
+});
+
+module.exports = router;
