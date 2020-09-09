823c9113de76974ac6ca35c6ddef85579594f0c1
Emmanuel <eherdezmoran@gmail.com>
2020-09-08 20:15:34 -0500

    Module 3 done

--- src/app.js
+++ src/app.js
@@ -7,10 +7,34 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, '/public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json', { encoding: "UTF8" });
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', { encoding: "UTF8" });
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
+
     res.render('index', {
-        title: 'Index'
+        title: 'Account Summary',
+        accounts
     });
+});
+
+app.get('/savings', (req, res) => {
+    res.render('account', { account: accounts.savings });
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', { account: accounts.checking });
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', { account: accounts.credit });
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', { user: users[0] });
 });
 
 
