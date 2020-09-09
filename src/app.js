de82d1ac585ef04bedef116fa70f6e14ea0a5cfb
Emmanuel <eherdezmoran@gmail.com>
2020-09-08 19:20:37 -0500

    activity 3

--- src/app.js
+++ src/app.js
@@ -4,4 +4,4 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set(path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
